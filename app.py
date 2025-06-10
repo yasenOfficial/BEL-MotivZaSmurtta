08a4fb04ce50150317266af120177dd86ef6b11d
yasenOfficial <yasentsv@gmail.com>
2025-06-10 18:26:28 +0300

    change port

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def get_theme_config():
     return jsonify(theme_config)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000) 
+    app.run(debug=True, host="0.0.0.0", port=7000) 
